8d4d1809a9836938246af751227c848dbc2cdb17
mbozkurt <mbozkurt@migros.com.tr>
2025-04-16 18:33:56 +0300

    adjust RedisTestConfiguration

--- src/test/java/mb/oauth2authorizationserver/config/RedisTestConfiguration.java
+++ src/test/java/mb/oauth2authorizationserver/config/RedisTestConfiguration.java
@@ -1,6 +1,9 @@
 package mb.oauth2authorizationserver.config;
 
 import lombok.extern.slf4j.Slf4j;
+import mb.oauth2authorizationserver.exception.BaseException;
+import mb.oauth2authorizationserver.exception.OAuth2AuthorizationServerServiceErrorCode;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -16,14 +19,13 @@ public class RedisTestConfiguration {
     @Bean
     @ServiceConnection(name = "redis")
     public GenericContainer<?> redisContainer() {
-        GenericContainer<?> redisContainer = new GenericContainer<>(DockerImageName.parse("redis:7.4.1"))
-                .withExposedPorts(REDIS_PORT)
-                .withReuse(true);
-
-        redisContainer.start();
-
-        log.info("Redis server started. isCreated: {}, isRunning: {}", redisContainer.isCreated(), redisContainer.isRunning());
-
-        return redisContainer;
+        try (GenericContainer<?> redisContainer = new GenericContainer<>(DockerImageName.parse("redis:7.4.1"))) {
+            redisContainer.withExposedPorts(REDIS_PORT).withReuse(true).start();
+            log.info("Redis server started. isCreated: {}, isRunning: {}", redisContainer.isCreated(), redisContainer.isRunning());
+            return redisContainer;
+        } catch (Exception e) {
+            log.error("Failed to start Redis container. redisContainer - Exception: {}", ExceptionUtils.getStackTrace(e));
+            throw new BaseException(OAuth2AuthorizationServerServiceErrorCode.UNEXPECTED_ERROR);
+        }
     }
 }
