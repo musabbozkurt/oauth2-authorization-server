41bed8448344e08cc9f5848ec9aa1bc2a54d61c9
musabbozkurt <b_musab@hotmail.com>
2025-07-27 11:24:02 +0300

    Merge pull request #44
    * add-oracle-test-container

---
+++ src/test/java/mb/oauth2authorizationserver/data/repository/AuthorityRepositoryTest.java
@@ -0,0 +1,46 @@
+package mb.oauth2authorizationserver.data.repository;
+
+import mb.oauth2authorizationserver.config.OracleTestConfiguration;
+import mb.oauth2authorizationserver.data.entity.Authority;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.instancio.Select.field;
+
+@DataJpaTest(showSql = false)
+@Import(OracleTestConfiguration.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestPropertySource(properties = "spring.flyway.enabled=false")
+class AuthorityRepositoryTest {
+
+    @Autowired
+    private AuthorityRepository authorityRepository;
+
+    @BeforeAll
+    void setUp() {
+        authorityRepository.saveAll(
+                Instancio.ofList(Authority.class)
+                        .size(10)
+                        .supply(field(Authority::getId), () -> null)  // 👈 ensure ID is null
+                        .create()
+        );
+    }
+
+    @Test
+    void findAll_ShouldReturnTenAuthorities_WhenPersisted() {
+        assertThat(authorityRepository.findAll())
+                .hasSize(10)
+                .allSatisfy(authority -> {
+                            assertThat(authority.getId()).isNotNull();
+                            assertThat(authority.getAuthority()).isNotBlank();
+                        }
+                );
+    }
+}
